c86998d1c182b3839df090c92578dfb03f54309c
liliyom <liliyoumkarim@gmail.com>
2023-02-01 12:16:14 +0500

    my first app

---
+++ index.js
@@ -0,0 +1,34 @@
+const http = require("http");
+const fs = require("fs");
+var requests = require("requests"); 
+
+const homeFile = fs.readFileSync("home.html", "utf-8");
+
+const replaceVal = (tempVal,orgVal) => {
+    let temperature = tempVal.replace("{% tempval %}", orgVal.main.temp);
+     temperature = temperature.replace("{% tempmin %}", orgVal.main.temp_min);
+     temperature = temperature.replace("{% tempmax %}", orgVal.main.temp_max);
+     temperature = temperature.replace("{% location %}", orgVal.name);
+     temperature = temperature.replace("{% country %}", orgVal.sys.country);
+     temperature = temperature.replace("{% tempstatus %}", orgVal.weather[0].main);
+    return temperature;
+}
+const server = http.createServer((req,res) => {
+if (req.url == "/") {
+    requests("https://api.openweathermap.org/data/2.5/weather?q=Gilgit&appid=f6bf189aba4c2fd066abcdf08277a8d5&units=metric")
+    .on('data',  (chunk) => {
+        const objdata = JSON.parse(chunk);
+        const arrdata = [objdata];
+    //   console.log(arrdata[0].main.temp)
+    const realTimeData = arrdata.map((val)=> replaceVal(homeFile, val))
+    .join("");
+    res.write(realTimeData);
+    })
+    .on('end',  (err) => {
+      if (err) return console.log('connection closed due to errors', err);
+     res.end();
+
+    });
+}
+});
+server.listen(8000, "127.0.0.1")
